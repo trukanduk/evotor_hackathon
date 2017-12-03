40b7b0f74dba4dd9e109fc8ccc5951d006f28f61
Ilya Uvarenkov <trukanduk@gmail.com>
2017-12-03 03:44:51 +0300

    sorting of items

---
+++ evotor/shop/templatetags/templateutils.py
@@ -0,0 +1,13 @@
+from django import template
+import json
+from django.utils.safestring import mark_safe
+
+register = template.Library()
+
+@register.filter(name="emptylist", is_safe=False, autoescape=False, needs_autoescape=True)
+def emptylist(datum, autoescape=False):
+    return mark_safe(datum or '[]')
+
+@register.filter(name="emptydict", is_safe=False, autoescape=False, needs_autoescape=True)
+def emptydict(datum, autoescape=False):
+    return mark_safe(datum or '{}')
--- evotor/static/js/shop/items.js
+++ evotor/static/js/shop/items.js
@@ -3,10 +3,53 @@ function request_items(model, template, filters) {
     $("#items-loading-img").show();
     $("#items-container-loading-error").hide();
     return query_html(model, template, filters)
-            .done(function(html) { $("#items-container").html(html); })
+            .done(function(html) {
+                $("#items-container").html(html);
+                update_items_icons();
+            })
             .fail(function(data) {
                 $("#item-container-loading-error-desc").html(data.content || "Неизвестная ошибка");
                 $("#items-container-loading-error").show();
             })
             .always(function() { $("#items-loading-img").hide(); });
 }
+
+function update_items_icons() {
+    $(".sort-items-pair").removeClass('active');
+    $(".sort-icon-desc").show();
+    $(".sort-icon-asc").hide();
+
+    if (itemsOrderByInfo.length == 0) {
+        return;
+    }
+
+    let orderInfo = itemsOrderByInfo[0];
+    let order = 'asc';
+    let column = orderInfo
+    if (orderInfo.substring(0, 1) == "-") {
+        order = 'desc';
+        column = orderInfo.substring(1);
+    }
+
+    $(".item-header-" + column + " .sort-icon").hide();
+    $(".item-header-" + column + " .sort-icon-" + order).show();
+    $(".item-header-" + column + " .sort-items-pair").addClass('active');
+}
+
+$(function() {
+    if (!itemsModelName) {
+        return;
+    }
+
+    $(".sort-icon").click(function() {
+        let order = ($(this).attr('order') == 'asc' ? '-' : '');
+        let column = $(this).attr('column');
+
+        let template = (typeof itemsTemplate === 'undefined' ? 'shop/' + itemsModelName + 's_items.html' : itemsTemplate);
+        request_items(
+                itemsModelName, template,
+                {filter: itemsFilterInfo, order_by: '["' + order + column + '"]'});
+    });
+    update_items_icons()
+});
+
