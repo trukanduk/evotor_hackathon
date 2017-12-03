dfc810f9886f875c08133e8f4bfbdf311731b8d3
Vladimir Nikolaev <vvnikolaev@yandex-team.ru>
2017-12-03 12:27:12 +0300

    fix upload real data

--- evotor/upload_real_data.py
+++ evotor/upload_real_data.py
@@ -20,20 +20,6 @@ from shop.models import (
 
 
 def kofe():
-    org = Organization.objects.get_or_create(title="Организация Кофейня")[0]
-    print(org)
-
-    if not User.objects.filter(username="kofeyana_owner").exists():
-        owner = User.objects.create_user("kofeyana_owner", "owner@kofe.ru", "123")
-        owner.first_name = "Иван"
-        owner.last_name = "Кофееввич"
-        owner.organization = org
-        owner.role = User.Role.ADMIN
-        owner.save()
-    else:
-        owner = User.objects.get(username="kofeyana_owner")
-
-
     if not Shop.objects.filter(organization=org).exists():
         shop = Shop.objects.create(
             organization=org,
@@ -43,7 +29,7 @@ def kofe():
     else:
         shop = Shop.objects.get(organization=org)
 
-    products = pd.read_csv("kofe.csv").values
+    products = pd.read_csv("newkofe.csv").values
 
     for product in products:
         if not Product.objects.filter(shop=shop, title=product).exists():
@@ -58,16 +44,6 @@ def kofe():
 
 
 def pivo():
-    org = Organization.objects.get_or_create(title="Организация Пиво")[0]
-    print(org)
-
-    owner = User.objects.create_user("pivo_owner", "owner@pivo.ru", "123")
-    owner.first_name = "Иван"
-    owner.last_name = "Пивовар"
-    owner.organization = org
-    owner.role = User.Role.ADMIN
-    owner.save()
-
     shop = Shop.objects.create(
         organization=org,
         title="Пиво",
@@ -88,23 +64,13 @@ def pivo():
                 count=int(product[4]))
 
 def alco():
-    org = Organization.objects.get_or_create(title="Организация Водка")[0]
-    print(org)
-
-    owner = User.objects.create_user("vodka_owner", "owner@alco.ru", "123")
-    owner.first_name = "Иван"
-    owner.last_name = "Водочный"
-    owner.organization = org
-    owner.role = User.Role.ADMIN
-    owner.save()
-
     shop = Shop.objects.create(
         organization=org,
         title="Алко",
         data_id=17846
     )
 
-    products = pd.read_csv("alco.csv").values
+    products = pd.read_csv("newalco.csv").values
 
     for product in products:
         if not Product.objects.filter(shop=shop, title=product).exists():
@@ -119,23 +85,13 @@ def alco():
 
 
 def regular():
-    org = Organization.objects.get_or_create(title="Продуктовый магазин")[0]
-    print(org)
-
-    owner = User.objects.create_user("product_owner", "owner@product.ru", "123")
-    owner.first_name = "Иван"
-    owner.last_name = "Продуктовый"
-    owner.organization = org
-    owner.role = User.Role.ADMIN
-    owner.save()
-
     shop = Shop.objects.create(
         organization=org,
         title="Продуктовый",
         data_id=17846
     )
 
-    products = pd.read_csv("prod.csv").values
+    products = pd.read_csv("newprod.csv").values
 
     for product in products:
         if not Product.objects.filter(shop=shop, title=product).exists():
@@ -149,23 +105,13 @@ def regular():
                 count=int(product[4]))
 
 def meat():
-    org = Organization.objects.get_or_create(title="Мясная лавка")[0]
-    print(org)
-
-    owner = User.objects.create_user("meat_owner", "owner@meat.ru", "123")
-    owner.first_name = "Иван"
-    owner.last_name = "Мяснов"
-    owner.organization = org
-    owner.role = User.Role.ADMIN
-    owner.save()
-
     shop = Shop.objects.create(
         organization=org,
         title="Мясной",
         data_id=17846
     )
 
-    products = pd.read_csv("meat.csv").values
+    products = pd.read_csv("newmeat.csv").values
 
     for product in products:
         if not Product.objects.filter(shop=shop, title=product).exists():
@@ -180,23 +126,13 @@ def meat():
 
 
 def larek():
-    org = Organization.objects.get_or_create(title="Ларек")[0]
-    print(org)
-
-    owner = User.objects.create_user("larek_owner", "owner@larek.ru", "123")
-    owner.first_name = "Иван"
-    owner.last_name = "Ларьковый"
-    owner.organization = org
-    owner.role = User.Role.ADMIN
-    owner.save()
-
     shop = Shop.objects.create(
         organization=org,
         title="Ларек",
         data_id=17846
     )
 
-    products = pd.read_csv("larek.csv").values
+    products = pd.read_csv("newlarek.csv").values
 
     for product in products:
         if not Product.objects.filter(shop=shop, title=product).exists():
@@ -208,6 +144,21 @@ def larek():
                 price=float(product[2]),
                 cost_price=float(product[3]),
                 count=int(product[4]))
+
+org = Organization.objects.get_or_create(title="Тестовая организация")[0]
+print(org)
+
+owner_filter = User.objects.filter(username="test")
+if owner_filter.exists():
+    owner = owner_filter[0]
+else:
+    owner = User.objects.create_user("test", "test@test.ru", "123")
+    owner.first_name = "Тестовый"
+    owner.last_name = "Аккаунт"
+    owner.organization = org
+    owner.role = User.Role.ADMIN
+    owner.save()
+
 
 kofe()
 pivo()
