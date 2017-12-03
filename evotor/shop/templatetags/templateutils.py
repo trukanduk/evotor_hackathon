8ef1055526854b49f365e29b44767a4c08c99824
Ilya Uvarenkov <trukanduk@gmail.com>
2017-12-03 12:12:19 +0300

    shop/

--- evotor/shop/templatetags/templateutils.py
+++ evotor/shop/templatetags/templateutils.py
@@ -23,3 +23,18 @@ def minusf(val, val2):
 @register.filter(name="marja")
 def marja(val1, val2):
     return 100.0 * (float(val1) - float(val2)) / (float(val1))
+
+@register.filter(name="status2bootstrap")
+def status2bootstrap(v):
+    if v < 10:
+        return 'error'
+    elif v < 90:
+        return 'success'
+    else:
+        return 'primary'
+
+@register.filter(name="getstatus")
+def getstatus(v, m):
+    v = float(v)
+    m, mm = map(int, m.split('-'))
+    return min(v, mm) - m
