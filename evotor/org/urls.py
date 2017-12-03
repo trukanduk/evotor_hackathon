8282be8b43fa38ce6c61bb046f77ad9bf6f9fd5d
Ilya Korovin <ilya.krvn@gmail.com>
2017-12-03 16:28:17 +0300

    collaborators add/delete

--- evotor/org/urls.py
+++ evotor/org/urls.py
@@ -3,7 +3,12 @@ import org.views
 
 urlpatterns = [
     url(r'^$', org.views.organization_index_view),
+    url(r'^collaborators/$', org.views.collaborators_view),
     url('^register_api/$', org.views.register_api_view),
     url('^activate/(?P<activation_id>[a-zA-Z0-9]{20,30})/$', org.views.activate_view),
+    url('^add_manager/$', org.views.add_manager_view),
+    url('^manager_rights/(?P<manager_id>[0-9]{1,})/$', org.views.manager_rights_view),
+    url('^manager_rights/(?P<manager_id>[0-9]{1,})/add/$', org.views.manager_rights_add_view),
+    url('^manager_rights/delete/(?P<right_id>[0-9]{1,})/$', org.views.manager_rights_delete_view),
     url('^', include('django.contrib.auth.urls')),
 ]
--- evotor/org/views.py
+++ evotor/org/views.py
@@ -8,6 +8,7 @@ from org.utils import (
     get_user_shops,
 )
 from shop.models import (
+    Shop,
     UserShop,
 )
 from org.models import (
@@ -29,9 +30,16 @@ def organization_index_view(request):
             us.shop.month_profit_type = "negative"
         else:
             us.shop.month_profit_type = "positive"
-    
+
     return render(request, "org/index.html", {
         "user_shops": user_shops,
+    })
+
+
+def collaborators_view(request):
+    managers = list(sorted(User.objects.filter(organization=request.user.organization), key=lambda u: u.full_name()))
+    return render(request, "org/collaborators.html", {
+        "managers": managers,
     })
 
 
@@ -91,3 +99,56 @@ def activate_view(request, activation_id):
 
     return render(request, "org/activation.html", {
     })
+
+
+def add_manager_view(request):
+    if request.method == "POST":
+        user = User.objects.create_user(
+            request.POST["login"],
+            request.POST["email"],
+            "123",
+        )
+
+        user.first_name = request.POST["first_name"]
+        user.last_name = request.POST["last_name"]
+        user.organization = request.user.organization
+        user.role = User.Role.MANAGER
+
+        user.save()
+
+        return redirect("/org/collaborators/")
+
+    return render(request, "org/add_manager.html", {})
+
+
+def manager_rights_view(request, manager_id):
+    manager = User.objects.get(id=manager_id)
+    shops = list(Shop.objects.filter(organization=request.user.organization))
+    rights = list(UserShop.objects.filter(user=manager))
+    
+    return render(request, "org/manager_rights.html", {
+        "manager": manager,
+        "shops": shops,
+        "rights": rights,
+    })
+
+
+def manager_rights_delete_view(request, right_id):
+    right = UserShop.objects.get(id=right_id)
+    manager = right.user
+    right.delete()
+    
+    return redirect("/org/manager_rights/{}/".format(manager.id))
+
+def manager_rights_add_view(request, manager_id):
+    manager = User.objects.get(id=manager_id)
+    shop = Shop.objects.get(id=int(request.POST["shop_id"]))
+    type = UserShop.Role.READ_WRITE
+
+    UserShop.objects.get_or_create(
+        user=manager,
+        shop=shop,
+        type=type,
+    )
+    
+    return redirect("/org/manager_rights/{}/".format(manager.id))
